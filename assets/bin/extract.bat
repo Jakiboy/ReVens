a29285eb7e3d83dcd2920c90ae43c735bb28c684
JIHAD SINNAOUR <j.sinnaour.official@gmail.com>
2023-07-17 14:27:15 +0100

    Rename

--- assets/bin/extract.bat
+++ assets/bin/extract.bat
@@ -1,5 +1,5 @@
-7z.exe x "./ReVen.zip.001" -y
-copy "./ReVen.bin" "./ReVen.iso"
-7z.exe x "./ReVen.iso" -y
-del \f "./ReVen.bin"
-del \f "./ReVen.zip.*"
+7z.exe x "./ReVens.zip.001" -y
+copy "./ReVens.bin" "./ReVens.iso"
+7z.exe x "./ReVens.iso" -y
+del \f "./ReVens.bin"
+del \f "./ReVens.zip.*"
