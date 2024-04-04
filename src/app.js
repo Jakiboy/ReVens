c55de1dad1f57096d5053150e68fe58f120d9b95
Jakiboy <j.sinnaour.official@gmail.com>
2024-04-04 16:53:12 +0000

    Dev

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+/**
+ * Author  : Jakiboy (Jihad Sinnaour)
+ * Package : ReVens | Reverse Engineering Toolkit AIO
+ * Version : 1.2.0
+ */
+
+'use strict';
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Launcher from './app/launcher';
