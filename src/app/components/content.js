3485fe28fc69da9d22d92b08f3effeda511be9d6
Jakiboy <j.sinnaour.official@gmail.com>
2024-04-04 00:59:29 +0000

    Removed static vendors

--- src/app/components/content.js
+++ src/app/components/content.js
@@ -5,6 +5,7 @@
  */
 
 import React from 'react';
+import { MDBBtn as Btn } from 'mdb-react-ui-kit';
 
 const Content = ({ activeTab }) => {
   return (
@@ -18,30 +19,30 @@ const Content = ({ activeTab }) => {
                 <h3 className="section-title">Sub section 1</h3>
                 <p className="section-description">Sub section 1 description.</p>
                 <div className="button-wrapper">
-                  <a href="#" className="btn btn-primary">
-                      <i className="icon-control-play"></i> Software 1
-                    </a>
-                    <a href="#" className="btn btn-primary archived">
-                      <i className="icon-folder"></i> Software 2
-                    </a>
-                    <a href="#" className="btn btn-primary cli">
-                      <i className="icon-frame"></i> Software 3
-                    </a>
+                  <Btn>
+                    <i className="icon-control-play"></i> Software 1
+                  </Btn>
+                  <Btn className="archived">
+                    <i className="icon-folder"></i> Software 2
+                  </Btn>
+                  <Btn className="cli">
+                    <i className="icon-frame"></i> Software 3
+                  </Btn>
                 </div>
               </div>
               <div className="section-wrapper">
                 <h3 className="section-title">Sub section 2</h3>
                 <p className="section-description">Sub section 2 description.</p>
                 <div className="button-wrapper">
-                    <a href="#" className="btn btn-primary">
+                    <Btn>
                       <i className="icon-control-play"></i> Software 4
-                    </a>
-                    <a href="#" className="btn btn-primary archived">
+                    </Btn>
+                    <Btn className="archived">
                       <i className="icon-folder"></i> Software 5
-                    </a>
-                    <a href="#" className="btn btn-primary cli">
+                    </Btn>
+                    <Btn className="cli">
                       <i className="icon-frame"></i> Software 6
-                    </a>
+                    </Btn>
                 </div>
               </div>
             </div>
@@ -57,60 +58,60 @@ const Content = ({ activeTab }) => {
                 <h3 className="section-title">Sub section 1</h3>
                 <p className="section-description">Sub section 1 description.</p>
                 <div className="button-wrapper">
-                  <a href="#" className="btn btn-primary">
+                  <Btn>
                     <i className="icon-control-play"></i> Software 1
-                  </a>
-                  <a href="#" className="btn btn-primary archived">
+                  </Btn>
+                  <Btn className="archived">
                     <i className="icon-folder"></i> Software 2
-                  </a>
-                  <a href="#" className="btn btn-primary cli">
+                  </Btn>
+                  <Btn className="cli">
                     <i className="icon-frame"></i> Software 3
-                  </a>
+                  </Btn>
                 </div>
               </div>
               <div className="section-wrapper">
                 <h3 className="section-title">Sub section 2</h3>
                 <p className="section-description">Sub section 2 description.</p>
                 <div className="button-wrapper">
-                  <a href="#" className="btn btn-primary">
+                  <Btn>
                     <i className="icon-control-play"></i> Software 4
-                  </a>
-                  <a href="#" className="btn btn-primary archived">
+                  </Btn>
+                  <Btn className="archived">
                     <i className="icon-folder"></i> Software 5
-                  </a>
-                  <a href="#" className="btn btn-primary cli">
+                  </Btn>
+                  <Btn className="cli">
                     <i className="icon-frame"></i> Software 6
-                  </a>
+                  </Btn>
                 </div>
               </div>
               <div className="section-wrapper">
                 <h3 className="section-title">Sub section 3</h3>
                 <p className="section-description">Sub section 2 description.</p>
                 <div className="button-wrapper">
-                  <a href="#" className="btn btn-primary">
+                  <Btn>
                     <i className="icon-control-play"></i> Software 7
-                  </a>
-                  <a href="#" className="btn btn-primary archived">
+                  </Btn>
+                  <Btn className="archived">
                     <i className="icon-folder"></i> Software 8
-                  </a>
-                  <a href="#" className="btn btn-primary cli">
+                  </Btn>
+                  <Btn className="cli">
                     <i className="icon-frame"></i> Software 9
-                  </a>
+                  </Btn>
                 </div>
               </div>
               <div className="section-wrapper">
                 <h3 className="section-title">Sub section 4</h3>
                 <p className="section-description">Sub section 2 description.</p>
                 <div className="button-wrapper">
-                  <a href="#" className="btn btn-primary">
+                  <Btn>
                     <i className="icon-control-play"></i> Software 10
-                  </a>
-                  <a href="#" className="btn btn-primary archived">
+                  </Btn>
+                  <Btn className="archived">
                     <i className="icon-folder"></i> Software 11
-                  </a>
-                  <a href="#" className="btn btn-primary cli">
+                  </Btn>
+                  <Btn className="cli">
                     <i className="icon-frame"></i> Software 12
-                  </a>
+                  </Btn>
                 </div>
               </div>
             </div>
--- src/app/components/head.js
+++ src/app/components/head.js
@@ -7,9 +7,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import config from '../../config/app.json';
-import '../assets/vendor/mdb/mdb.dark.min.css';
-import '../assets/vendor/simple-line-icons/simple-line-icons.min.css';
-import '../assets/css/font.css';
+import 'mdb-react-ui-kit/dist/css/mdb.dark.min.css';
+import 'simple-line-icons/css/simple-line-icons.css';
 import '../assets/css/app.css';
 
 const Head = () => (
