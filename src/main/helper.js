02be0d67f0497ff0bad20bc9ae4681c793e744dd
Jakiboy <j.sinnaour.official@gmail.com>
2024-04-06 07:59:42 +0000

    Update helper.js

--- src/main/helper.js
+++ src/main/helper.js
@@ -8,9 +8,9 @@ const { shell, dialog, Notification } = require('electron');
 const config = require('../config/app.json');
 const path = require('path');
 const url = require('url');
-const fs = require('fs');
-const Axios = require('axios');
-const Progress = require('electron-progressbar');
+// const fs = require('fs');
+// const Axios = require('axios');
+// const Progress = require('electron-progressbar');
 
 /**
  * Setup env.
@@ -80,6 +80,22 @@ function openBinFolder() {
 }
 
 /**
+ * Open info.
+ */
+function openInfo() {
+	const md = getRoot('/') + 'ReVens.md';
+	shell.openPath(md);
+}
+
+/**
+ * Open changelog.
+ */
+function openChangelog() {
+	const c = getRoot('/') + 'changelog.txt';
+	shell.openPath(c);
+}
+
+/**
  * Get path.
  */
 function getPath(file) {
@@ -100,171 +116,155 @@ function getRoot(dir) {
 /**
  * Notify.
  */
-function notify(msg) {
-	if (Notification.isSupported()) {
-		new Notification({
-			"title": 'ReVens',
-			"body": msg,
-			"icon": path.join(__dirname, '../app/assets/img/icon.png')
-		}).show();
-	}
-}
+// function notify(msg) {
+// 	if (Notification.isSupported()) {
+// 		new Notification({
+// 			"title": 'ReVens',
+// 			"body": msg,
+// 			"icon": path.join(__dirname, '../app/assets/img/icon.png')
+// 		}).show();
+// 	}
+// }
 
 /**
  * Download packages.
  */
-function downloadPackages() {
+// function downloadPackages() {
 
-	// Confirm
-	const response = dialog.showMessageBoxSync(win, {
-		"title": 'ReVens',
-		"buttons": ['No', 'Yes'],
-		"message": 'Do you want to download packages?'
-	});
-	if (response !== 1) { return; }
+// 	// Confirm
+// 	const response = dialog.showMessageBoxSync(win, {
+// 		"title": 'ReVens',
+// 		"buttons": ['No', 'Yes'],
+// 		"message": 'Do you want to download packages?'
+// 	});
+// 	if (response !== 1) { return; }
 
-	// Download
-	displayProgress();
+// 	// Download
+// 	displayProgress();
 	
-	const config = formatPath(
-		path.join(__dirname, '/config/app.json')
-	);
+// 	const config = formatPath(
+// 		path.join(__dirname, '/config/app.json')
+// 	);
 
-	fs.readFile(config, "utf8", (err, json) => {
-		if (!err) {
+// 	fs.readFile(config, "utf8", (err, json) => {
+// 		if (!err) {
 			
-			const c = JSON.parse(json);
-			let packages = [], host;
+// 			const c = JSON.parse(json);
+// 			let packages = [], host;
 
-			packages = c.packages;
-			host = c.host;
+// 			packages = c.packages;
+// 			host = c.host;
 
-			if ( !packages || !host ) {
-				notify('Download failed: Invalid config');
-				return;
-			}
+// 			if ( !packages || !host ) {
+// 				notify('Download failed: Invalid config');
+// 				return;
+// 			}
 
-			let part = 0;
-			packages.forEach(function(file) {
-				part++;
-				const url = `${host}/${file}`;
-				download(url, file, part, packages.length);
-			});
-		}
-	});
-}
+// 			let part = 0;
+// 			packages.forEach(function(file) {
+// 				part++;
+// 				const url = `${host}/${file}`;
+// 				download(url, file, part, packages.length);
+// 			});
+// 		}
+// 	});
+// }
 
 /**
  * Async download.
  */
-async function download(url, file, part, count) {
+// async function download(url, file, part, count) {
 
-	// Init
-	const response = await Axios({
-		"url": url,
-		"method": 'GET',
-		"responseType": 'stream'
-	});
+// 	// Init
+// 	const response = await Axios({
+// 		"url": url,
+// 		"method": 'GET',
+// 		"responseType": 'stream'
+// 	});
 
-	// Pipe
-	let path = getRoot('bin') + file;
-	path = formatPath(path);
-	response.data.pipe(fs.createWriteStream(path));
+// 	// Pipe
+// 	let path = getRoot('bin') + file;
+// 	path = formatPath(path);
+// 	response.data.pipe(fs.createWriteStream(path));
 
-	// End
-	response.data.on('end', () => {
-		extractPackage(file);
-		if ( part == count ) {
-			complateProgress();
-			notify('Packages succesfully downloaded');
-		}
-	});
+// 	// End
+// 	response.data.on('end', () => {
+// 		extractPackage(file);
+// 		if ( part == count ) {
+// 			complateProgress();
+// 			notify('Packages succesfully downloaded');
+// 		}
+// 	});
 
-	// Error
-	response.data.on('error', (error) => {
-		notify(`Downloaded failed: ${error}`);
-		complateProgress();
-	});
-}
+// 	// Error
+// 	response.data.on('error', (error) => {
+// 		notify(`Downloaded failed: ${error}`);
+// 		complateProgress();
+// 	});
+// }
 
 /**
  * Display progress.
  */
-function displayProgress() {
+// function displayProgress() {
 
-	if ( bar ) {
-		bar.close();
-		bar = null;
-	}
+// 	if ( bar ) {
+// 		bar.close();
+// 		bar = null;
+// 	}
 	
-	bar = new Progress({
-		"browserWindow": {
-			"parent": win,
-			"modal": true,
-			"icon": path.join(__dirname, './app/assets/img/icon.ico'),
-			'webPreferences': {
-            	'nodeIntegration': false
-        	}
-		},
-		"title": 'ReVens',
-		"text": 'Downloading packages...',
-		"detail": 'Please wait',
-		"style": {
-			"value": {
-				"background-color": '#ff8636'
-			}
-		}
-	});
+// 	bar = new Progress({
+// 		"browserWindow": {
+// 			"parent": win,
+// 			"modal": true,
+// 			"icon": path.join(__dirname, './app/assets/img/icon.ico'),
+// 			'webPreferences': {
+//             	'nodeIntegration': false
+//         	}
+// 		},
+// 		"title": 'ReVens',
+// 		"text": 'Downloading packages...',
+// 		"detail": 'Please wait',
+// 		"style": {
+// 			"value": {
+// 				"background-color": '#ff8636'
+// 			}
+// 		}
+// 	});
 	
-	bar.on('completed', function() {
-		bar.detail = 'Download completed. Exiting...';
-		bar.close();
-		bar = null;
-	});
-}
+// 	bar.on('completed', function() {
+// 		bar.detail = 'Download completed. Exiting...';
+// 		bar.close();
+// 		bar = null;
+// 	});
+// }
 
 /**
  * Complate progress.
  */
-function complateProgress () {
-	if ( bar ) {
-		bar.setCompleted();
-	}
-}
+// function complateProgress () {
+// 	if ( bar ) {
+// 		bar.setCompleted();
+// 	}
+// }
 
 /**
  * Extract package.
  */
-function extractPackage(file) {
-	const name = file.replace('.iso', '');
-	const bat = getRoot('bin') + `${name}.bat`;
+// function extractPackage(file) {
+// 	const name = file.replace('.iso', '');
+// 	const bat = getRoot('bin') + `${name}.bat`;
 
-	let cmd  = `7z.exe x "./${name}.iso" -y`;
-	    cmd += "\n";
-	    cmd += `del \\f "./${name}.iso"`;
+// 	let cmd  = `7z.exe x "./${name}.iso" -y`;
+// 	    cmd += "\n";
+// 	    cmd += `del \\f "./${name}.iso"`;
 
-	fs.writeFile(bat, cmd, function (err) {});
+// 	fs.writeFile(bat, cmd, function (err) {});
 
-	setTimeout(function() {
-		shell.openPath(bat);
-	}, 2000);
-}
-
-/**
- * Open info.
- */
-function openInfo() {
-	const md = getRoot('/') + 'ReVens.md';
-	shell.openPath(md);
-}
-
-/**
- * Open changelog.
- */
-function openChangelog() {
-	const c = getRoot('/') + 'changelog.txt';
-	shell.openPath(c);
-}
+// 	setTimeout(function() {
+// 		shell.openPath(bat);
+// 	}, 2000);
+// }
 
 module.exports = {
     setup,
