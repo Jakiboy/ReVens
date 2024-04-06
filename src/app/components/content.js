f5625a6f11eaa8bd6c6b669c7779615be7b30a98
Jakiboy <j.sinnaour.official@gmail.com>
2024-04-06 02:24:44 +0000

    Dev

--- src/app/components/content.js
+++ src/app/components/content.js
@@ -5,11 +5,11 @@
  */
 
 import React from 'react';
-import { MDBBtn as Btn } from 'mdb-react-ui-kit';
-import { generateSlug } from '../helper';
 import strings from './../../config/strings.json';
 import sConfig from './../../config/sections.json';
-import pConfig from './../../config/packages.json';
+import iConfig from './../../config/items.json';
+import Section from './section';
+import { generateSlug } from '../helper';
 
 const Content = ({ activeTab }) => {
 
@@ -17,7 +17,7 @@ const Content = ({ activeTab }) => {
     section => generateSlug(section.name) === activeTab
   );
 
-  if ( !activeSection ) {
+  if (!activeSection) {
     return (
       <div className="tab-content has-error">
         <p>
@@ -28,41 +28,7 @@ const Content = ({ activeTab }) => {
     );
   }
 
-  const _section = generateSlug(activeSection.name);
-  const _subs = activeSection.subs;
-  const _packages = pConfig.packages;
-
-  return (
-    <div className="tab-content" id={`tab-content-${_section}`}>
-        <div className="tab-pane fade show active" id={_section} role="tabpanel" aria-labelledby={`${_section}-tab`}>
-            <p dangerouslySetInnerHTML={{ __html: activeSection.description }}></p>
-            <div className="section-container app-scroller">
-              {_subs.map((sub, index) => {
-                const _sub = generateSlug(sub.title);
-                return (
-                  <div className="section-wrapper" key={index}>
-                      <h3 className="section-title">{sub.title}</h3>
-                      <p className="section-description" dangerouslySetInnerHTML={{ __html: sub.description }}></p>
-                      <div className="button-wrapper">
-                        {_packages.map((file, index) => {
-                          const _fsection = generateSlug(file.section);
-                          const _fsub = generateSlug(file.sub);
-                          if ( (_fsection === _section) && (_fsub === _sub) ) {
-                            return (
-                              <Btn key={index}>
-                                <i className={`icon-${file.icon}`}></i> {file.name}
-                              </Btn>
-                            );
-                          }
-                        })}
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-        </div>
-    </div>
-  );
+  return <Section section={activeSection} items={iConfig.items} />;
 };
 
 export default Content;
