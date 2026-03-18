e3c83c939b9986bd232cb2f69d06db97e947dfbe
Jakiboy <j.sinnaour.official@gmail.com>
2026-03-18 09:36:38 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,8 +8,20 @@ npm i html-minifier -g
 npm i webpack -g
 npm i webpack-cli -g
 
+if [ ! -f "./inc/7z.exe" ]; then
+    echo "Download 7z..."
+    mkdir -p "./inc"
+    git clone "https://github.com/Jakiboy/7z.git/" "./inc/temp"
+    mv "./inc/temp/bin/7z.exe" "./inc/7z.exe"
+    mv "./inc/temp/bin/7z.dll" "./inc/7z.dll"
+    rm -rf "./inc/temp"
+    sleep 2
+    clear
+fi
+
 git config core.ignorecase false
 
 cd src
 npm install
+
 # export NODE_ENV=dev
